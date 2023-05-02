477326249fcc8480435f4c56e0da8ee8889d8b31
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-05-02 18:31:22 +0600

    added json file

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+const chefRecipe = require('./data/chefRecipe.json');
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("assignment ten server site");
 })
+app.get('/chefrecipe',(req, res) => {
+    res.send(chefRecipe);
+})
+
 
 
 app.listen(port, () => {
