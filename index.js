c9c5514afd534fa33649a73d6fd79e4385bf277b
MohammadShahansha <shahansha.cou.ict11@gmail.com>
2023-05-02 20:30:46 +0600

    finding selected id

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get('/chefrecipe',(req, res) => {
     res.send(chefRecipe);
 })
 
+app.get('/chefrecipe/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedChef = chefRecipe.find(chef => chef.id==id)
+    res.send(selectedChef);
+})
+
 
 
 app.listen(port, () => {
